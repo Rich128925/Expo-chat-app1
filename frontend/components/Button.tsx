e0f3f7807143404cdb577a686d0db230af245cdf
Godswill <sgodswill05@gmail.com>
2026-03-29 17:36:19 -0700

    Fix nested frontend repo issue

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,61 @@
+import React, { ReactNode } from "react";
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+import { colors, radius } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import Loading from "./Loading";
+
+type CustomButtonProps = {
+  children: ReactNode;
+  onPress?: () => void;
+  style?: StyleProp<ViewStyle>;
+  loading?: boolean;
+};
+
+const Button = ({
+  style,
+  onPress,
+  children,
+  loading = false,
+}: CustomButtonProps) => {
+  if (loading) {
+    return (
+      <View style={[styles.button, { backgroundColor: "transparent" }, style]}>
+        <Loading />
+      </View>
+    );
+  }
+
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
+      {typeof children === "string" ? (
+        <Text style={styles.text}>{children}</Text>
+      ) : (
+        children
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: radius.full,
+    borderCurve: "continuous",
+    height: verticalScale(56),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: colors.white,
+    fontWeight: "600",
+  },
+});
