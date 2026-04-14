05fefdc909837e44b9777718562cecbe4ccc6617
Godswill <sgodswill05@gmail.com>
2026-04-14 05:50:23 -0700

    feat: add configuration constants for API endpoints and Cloudinary integration

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,5 +1,6 @@
-// frontend/constants/index.ts
-export const API_URL = "http://10.152.165.192:4000";
+export const LOCAL_API_URL = "http://10.152.165.192:4000";
+export const PROD_API_URL = "https://expo-chat-app1.vercel.app";
+export const API_URL = __DEV__ ? LOCAL_API_URL : PROD_API_URL;
 
 export const CLOUDINARY_CLOUD_NAME = 'dxtdvnwvf';
 export const CLOUDINARY_UPLOAD_PRESET = 'images';
