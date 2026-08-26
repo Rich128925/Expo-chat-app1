8d3c39973bcbcf7571816f3ce874b38f7a823e0d
God'swill <sgodswill05@gmail.com>
2026-08-26 13:17:07 -0700

    feat: initialize backend server structure and frontend configuration constants

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // socket setup
 
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -2,7 +2,7 @@
 // Use YOUR actual IP from ipconfig
 const YOUR_COMPUTER_IP = "10.221.193.192";
 
-export const API_URL = `http://${YOUR_COMPUTER_IP}:4000`;
+export const API_URL = `http://${YOUR_COMPUTER_IP}:5000`;
 
 export const CLOUDINARY_CLOUD_NAME = 'dxtdvnwvf'
 export const CLOUDINARY_UPLOAD_PRESET = 'images'
